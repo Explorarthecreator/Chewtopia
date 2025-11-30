6bc5d9dee0fc98fe18664e978a120fd61851a3ee
Explorarthecreator <fredadejiemma99@gmail.com>
2025-11-30 18:45:13 +0100

    feat: add new font

--- app/font.ts
+++ app/font.ts
@@ -1,6 +1,11 @@
-import { Nunito } from "next/font/google";
+import { Baloo_2, Nunito } from "next/font/google";
 
 export const nunito = Nunito({
+  subsets: ["latin"],
+  style: "normal",
+});
+
+export const baloo = Baloo_2({
   subsets: ["latin"],
   style: "normal",
 });
